f2c6266bea0fa5ac72465b6fb3412227a774d2a2
Camillaatek <69843303+Camillaatek@users.noreply.github.com>
2021-04-25 22:47:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
+const url = "https://camillaatek.no/wp-json/wc/store/products/"
 const results = document.querySelector(".results")
 
-const flower = "https://camillaatek.no/wp-json/wc/store/products/"
-
-fetch(flower, {
-
-})
-
-
-    .then(response => response.json())
-    .then(data => productTemplate(data))
-    .catch(error => results.innerHTML = "ups that went wrong");
-
-
-const productTemplate =(products)=>{
+const productList =(products)=>{
     results.innerHTML = "";
     for(product of products){
-        console.log(product)
+        
         let productDiv = ``
-        // for(image of product.images){
-        //     productDiv += `
-        //     <ul class="card">
-        //     <a href="single-product.html?id=${product.id}"><img scr="${image.src}"></a>
-        //     <li>${product.name}</li>
-        //     <p>${product.prices.price}</p>
-        //     <button>View</button>
-        //     </ul>
-        //     `
-        // }
+        console.log(product)
         for (image of product.images){
             productDiv += `
             <ul class="card">
@@ -41,43 +21,7 @@ const productTemplate =(products)=>{
         results.innerHTML += productDiv
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const url = "http://camillaatek.no/wp-json/wc/store/products/"
-const container = document.querySelector('div')
-
-
-
- const productsList = (products) => {
-    console.log(product)
-    container.innerHTML = "";
-    for (product of products) {
-        
-        let nyFilm = `
-        <a href="./details.html?id=${i.id}"</ul>
-    <ul>
-    <h2>${i.title}</h2>
-    
-    
-    </ul>`;
-        container.innerHTML += nyFilm;
-    };
-};
-
- fetch(url)
+fetch(url)
     .then(response => response.json())
-    .then(data => productsList(data))
-    .catch(err => console.error(err.message));*/
+    .then(data => productList(data))
+    .catch(error => results.innerHTML = "ups that went wrong");
